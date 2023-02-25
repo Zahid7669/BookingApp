ca65a27eaacb1f2ff61f6e565790c8ad87b30c8d
S M Zahid Hasan <zahid.hasan.0109@gmail.com>
2023-02-25 19:30:21 +0200

    Created controller and route for room

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,21 @@
 import express from "express";
+import { createRoom, deleteRoom, getRoomById, getRooms, updateRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
 
+//CREATE
+router.post("/:hotelId",verifyAdmin, createRoom)
+
+//UPDATE
+router.put("/:id",verifyAdmin, updateRoom )
+
+//DELETE
+router.delete("/:id/:hotelId",verifyAdmin, deleteRoom)
+
+//GET
+router.get("/:id",getRoomById)
+
+//GETALL
+router.get("/",getRooms)
 
 export default router
